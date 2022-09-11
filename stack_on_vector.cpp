3b66620b5e5a3eda098f897340b351865d47f7c0
Ilya Kobyzev <KobyzevIlya@gmail.com>
2022-09-11 17:41:59 +0300

    version 1

--- stack_on_vector.cpp
+++ stack_on_vector.cpp
@@ -15,3 +15,30 @@ private:
     std::vector<int> body;
 };
 
+void Stack::push(int value) {
+    body.push_back(value);
+}
+
+int Stack::pop() {
+    if (body.size() == 0) {
+        throw "error";
+    }
+    int last_element = static_cast<int>body.rbegin();
+    body.pop_back()
+    return last_element;
+}
+
+int Stack::back() const {
+    if (body.size() == 0) {
+        throw "error";
+    }
+    return static_cast<int>body.rbegin();
+}
+
+size_t Stack::size() const {
+    return body.size();
+}
+
+void Stack::clear() {
+    body.clear();
+}
